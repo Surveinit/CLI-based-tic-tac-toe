d36a4d0718bddf7d635a6c0f38bda6f747dacb3e
Surve <surve8729@gmail.com>
2024-08-02 20:24:07 +0530

    Implement Victory logic

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative "board"
+require_relative "victory_checker"
 
 while true
 
@@ -8,12 +9,18 @@ while true
   print ">Choose your move X: "
   move = gets.chomp.to_i
   Board.XOBoard(move, 'X')
+  
+  if VictoryCheck.check_win('X') == true
+    return puts "X won"
+  end
+  
   puts "-----------"
 
   # Player 2  
   print ">Choose your move O: "
   move = gets.chomp.to_i
   Board.XOBoard(move, 'O')
+  VictoryCheck.check_win('X')
   puts "-----------"  
 
 end
---
+++ victory_checker.rb
@@ -0,0 +1,26 @@
+require_relative "board"
+
+class VictoryCheck
+
+  WIN_PATTERNS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+  def self.check_win(symbol)
+    flattened_board = Board.flatten_board
+
+    WIN_PATTERNS.each do |pattern|
+      if pattern.all? {|index| flattened_board[index] == symbol} 
+        return true
+      end
+    end
+  end
+  false
+end
