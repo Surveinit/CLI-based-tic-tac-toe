01d08b4f218f00a457acc022b07fcf7abbaed18b
Surve <surve8729@gmail.com>
2024-08-02 23:30:37 +0530

    Feature complete

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require 'colorize'
 require_relative "board"
 require_relative "victory_checker"
 
@@ -6,23 +7,23 @@ Board.dummy_board
 while true
 
   # Player 1
-  print ">Choose your move X: "
+  print ">Choose your move X: ".colorize(:green)
   move = gets.chomp.to_i
   Board.XOBoard(move, 'X')
   
   if VictoryCheck.check_win('X') == true
-    puts "X won"
+    puts "~ X won ~".colorize(:yellow)
     break
   end
   
 
   # Player 2  
-  print ">Choose your move O: "
+  print ">Choose your move O: ".colorize(:red)
   move = gets.chomp.to_i
   Board.XOBoard(move, 'O')
   
   if VictoryCheck.check_win('O') == true
-    puts "O won"
+    puts "~ O won ~".colorize(:yellow)
     break
   end
 
