dbc79d7625a7245db1a09206671799b56d5eb736
Surve <surve8729@gmail.com>
2024-09-17 19:26:46 +0530

    Write Y row tests for check_win

--- spec/victory_checker_spec.rb
+++ spec/victory_checker_spec.rb
@@ -24,5 +24,26 @@ describe VictoryCheck do
       end
 
     end
+
+    context 'board reads O O O across rows' do
+      let(:board) {instance_double(Board)}
+      subject(:victory_check) { described_class.new(board) }
+
+      it 'with top row' do
+        allow(board).to receive(:flatten_board).and_return(['O', 'O', 'O', 4, 5, 6, 7, 8, 9])
+        expect(victory_check.check_win('O')).to eq(true)
+      end
+      
+      it 'with middle row' do
+        allow(board).to receive(:flatten_board).and_return([1, 2, 3, 'O', 'O', 'O', 7, 8, 9])
+        expect(victory_check.check_win('O')).to eq(true)
+      end
+      
+      it 'with bottom row' do
+        allow(board).to receive(:flatten_board).and_return([1, 2, 3, 4, 5, 6, 'O', 'O', 'O'])
+        expect(victory_check.check_win('O')).to eq(true)
+      end
+
+    end
   end
 end
