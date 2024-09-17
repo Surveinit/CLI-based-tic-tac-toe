a1b4431b9331980eed5c933221c0485d22527fe9
Surve <surve8729@gmail.com>
2024-09-17 19:48:40 +0530

    Write X & 0 column tests for check_win

--- spec/victory_checker_spec.rb
+++ spec/victory_checker_spec.rb
@@ -4,6 +4,7 @@ require_relative '../lib/board'
 describe VictoryCheck do
 
   describe 'victory_checker' do
+
     context 'board reads X X X across rows' do
       let(:board) {instance_double(Board)}
       subject(:victory_check) { described_class.new(board) }
@@ -45,5 +46,48 @@ describe VictoryCheck do
       end
 
     end
+
+    context 'board reads X X X across columns' do
+      let(:board) {instance_double(Board)}
+      subject(:victory_check) { described_class.new(board) }
+
+      it 'with top row' do
+        allow(board).to receive(:flatten_board).and_return(['X', 2, 3, 'X', 5, 6, 'X', 8, 9])
+        expect(victory_check.check_win('X')).to eq(true)
+      end
+      
+      it 'with middle row' do
+        allow(board).to receive(:flatten_board).and_return([1, 'X', 3, 4, 'X', 6, 7, 'X', 9])
+        expect(victory_check.check_win('X')).to eq(true)
+      end
+      
+      it 'with bottom row' do
+        allow(board).to receive(:flatten_board).and_return([1, 2, 'X', 4, 5, 'X', 7, 8, 'X'])
+        expect(victory_check.check_win('X')).to eq(true)
+      end
+
+    end
+
+    context 'board reads O O O across columns' do
+      let(:board) {instance_double(Board)}
+      subject(:victory_check) { described_class.new(board) }
+
+      it 'with top row' do
+        allow(board).to receive(:flatten_board).and_return(['0', 2, 3, '0', 5, 6, '0', 8, 9])
+        expect(victory_check.check_win('0')).to eq(true)
+      end
+      
+      it 'with middle row' do
+        allow(board).to receive(:flatten_board).and_return([1, '0', 3, 4, '0', 6, 7, '0', 9])
+        expect(victory_check.check_win('0')).to eq(true)
+      end
+      
+      it 'with bottom row' do
+        allow(board).to receive(:flatten_board).and_return([1, 2, '0', 4, 5, '0', 7, 8, '0'])
+        expect(victory_check.check_win('0')).to eq(true)
+      end
+
+    end
+
   end
 end
